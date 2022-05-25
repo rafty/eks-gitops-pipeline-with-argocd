b0d1ff8589c60300d9cd5d908536b71db8b7e889
takashi <yagita.takashi@gmail.com>
2022-05-25 13:57:43 +0900

    Initial commit

---
+++ tests/unit/test_eks_gitops_pipeline_with_argocd_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from eks_gitops_pipeline_with_argocd.eks_gitops_pipeline_with_argocd_stack import EksGitopsPipelineWithArgocdStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in eks_gitops_pipeline_with_argocd/eks_gitops_pipeline_with_argocd_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = EksGitopsPipelineWithArgocdStack(app, "eks-gitops-pipeline-with-argocd")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
